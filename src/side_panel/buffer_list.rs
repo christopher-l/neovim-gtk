9b6802faa8646ed00f998758f518ad545826f6c5
Christopher Lübbemeier <christopher.luebbemeier@gmail.com>
2018-03-17 23:32:01 +0100

    Correctly recognize if paned was dragged

--- src/side_panel/buffer_list.rs
+++ src/side_panel/buffer_list.rs
@@ -1,3 +1,4 @@
+use std::cell::RefCell;
 use std::path::Path;
 use std::rc::Rc;
 
@@ -9,17 +10,36 @@ use neovim_lib::NeovimApi;
 use nvim::{NeovimClient, NeovimRef};
 use shell;
 
+struct PaneState {
+    min_height: usize,
+    offset: usize,
+    row_height: usize,
+    was_dragged: bool,
+}
+
 pub struct BufferList {
-    pub list: gtk::ListBox,
+    builder: gtk::Builder,
+    list: gtk::ListBox,
+    paned: gtk::Paned,
     nvim: Option<Rc<NeovimClient>>,
+    pane_state: Rc<RefCell<PaneState>>,
 }
 
 impl BufferList {
     pub fn new(builder: &gtk::Builder) -> Self {
         let list: gtk::ListBox = builder.get_object("buffer_list").unwrap();
+        let paned: gtk::Paned = builder.get_object("paned").unwrap();
         Self {
+            builder: builder.clone(),
             list,
+            paned,
             nvim: None,
+            pane_state: Rc::new(RefCell::new(PaneState {
+                min_height: 0,
+                offset: 0,
+                row_height: 0,
+                was_dragged: false,
+            })),
         }
     }
 
@@ -27,32 +47,77 @@ impl BufferList {
         let nvim = shell_state.nvim_clone();
         self.nvim = Some(nvim);
 
+        self.init_pane_state();
         self.init_subscriptions(&shell_state);
+        self.connect_events();
+    }
+
+    fn init_pane_state(&mut self) {
+        let pane_state = &mut self.pane_state.borrow_mut();
+        pane_state.min_height = self.paned.get_position() as usize;
+        let buffer_list_scroll: gtk::ScrolledWindow =
+            self.builder.get_object("buffer_list_scroll").unwrap();
+        let buffer_list_box: gtk::Box =
+            self.builder.get_object("buffer_list_box").unwrap();
+        pane_state.offset = buffer_list_box.get_allocated_height() as usize
+            - buffer_list_scroll.get_allocated_height() as usize;
     }
 
     fn init_subscriptions(&mut self, shell_state: &shell::State) {
         let list = &self.list;
+        let paned = &self.paned;
         let nvim_ref = self.nvim.as_ref().unwrap();
+        let pane_state_ref = &self.pane_state;
         let update_list = shell_state.subscribe(
             "DirChanged",
             &["getcwd()"],
-            clone!(list, nvim_ref => move |args| {
+            clone!(list, paned, nvim_ref, pane_state_ref => move |args| {
                 let cwd = Path::new(&args[0]);
-                populate_list(&list, &mut nvim_ref.nvim().unwrap(), &cwd);
+                populate_list(
+                    &list,
+                    &paned,
+                    &mut nvim_ref.nvim().unwrap(),
+                    &mut pane_state_ref.borrow_mut(),
+                    &cwd,
+                );
             }),
         );
         shell_state.run_now(&update_list);
+    }
+
+    fn connect_events(&mut self) {
     }
 }
 
 fn populate_list(
     list: &gtk::ListBox,
+    paned: &gtk::Paned,
     mut nvim: &mut NeovimRef,
+    pane_state: &mut PaneState,
     cwd: &Path,
 ) {
-    for widget in list.get_children() {
+    let rows = list.get_children();
+    if !pane_state.was_dragged {
+        if pane_state.row_height == 0 {
+            if let Some(row) = rows.get(0) {
+                pane_state.row_height = row.get_allocated_height() as usize;
+            }
+        }
+        let required_height = pane_state.row_height * rows.len() + pane_state.offset;
+        if (
+            required_height > pane_state.min_height
+            && paned.get_position() as usize != required_height
+        ) || (
+            required_height <= pane_state.min_height
+            && paned.get_position() as usize > pane_state.min_height
+        ) {
+            pane_state.was_dragged = true;
+        }
+    }
+    for widget in rows {
         list.remove(&widget);
     }
+    let mut n_buffers = 0;
     if let Ok(buffers) = nvim.list_bufs() {
         for buffer in buffers {
             let is_listed = buffer
@@ -61,6 +126,7 @@ fn populate_list(
                 .as_bool()
                 .unwrap();
             if let (true, Ok(name)) = (is_listed, buffer.get_name(&mut nvim)) {
+                n_buffers += 1;
                 let display_name = if name.is_empty() {
                     "[No Name]"
                 } else if let Some(rel_path) = Path::new(&name)
@@ -72,7 +138,6 @@ fn populate_list(
                 } else {
                     &name
                 };
-                println!("display_name: {:?}", display_name);
                 let builder = gtk::Builder::new_from_string(
                     include_str!("../../resources/buffer_list_row.ui"),
                 );
@@ -84,5 +149,12 @@ fn populate_list(
             }
         }
     }
+    if !pane_state.was_dragged {
+        let required_height = pane_state.row_height * n_buffers + pane_state.offset;
+        if  required_height > pane_state.min_height {
+            paned.set_position(required_height as i32);
+        } else {
+            paned.set_position(pane_state.min_height as i32);
+        }
+    }
 }
-
--- src/side_panel/mod.rs
+++ src/side_panel/mod.rs
@@ -1,20 +1,16 @@
 mod buffer_list;
 mod file_browser;
 
-use std::cmp::max;
 use std::ops::Deref;
 
 use gtk;
-use gtk::prelude::*;
 
 use shell;
 use self::buffer_list::BufferList;
 use self::file_browser::FileBrowser;
 
 pub struct SidePanelWidget {
-    builder: gtk::Builder,
     widget: gtk::Box,
-    paned: gtk::Paned,
     buffer_list: BufferList,
     file_browser: FileBrowser,
 }
@@ -23,13 +19,10 @@ impl SidePanelWidget {
     pub fn new() -> Self {
         let builder = gtk::Builder::new_from_string(include_str!("../../resources/side-panel.ui"));
         let widget: gtk::Box = builder.get_object("side_panel").unwrap();
-        let paned: gtk::Paned = builder.get_object("paned").unwrap();
         let buffer_list = BufferList::new(&builder);
         let file_browser = FileBrowser::new(&builder);
         SidePanelWidget {
-            builder,
             widget,
-            paned,
             buffer_list,
             file_browser,
         }
@@ -38,40 +31,6 @@ impl SidePanelWidget {
     pub fn init(&mut self, shell_state: &shell::State) {
         self.buffer_list.init(&shell_state);
         self.file_browser.init(&shell_state);
-        self.connect_events();
-    }
-
-    fn connect_events(&mut self) {
-        let buffer_list_scroll: gtk::ScrolledWindow =
-            self.builder.get_object("buffer_list_scroll").unwrap();
-        let buffer_list_box: gtk::Box =
-            self.builder.get_object("buffer_list_box").unwrap();
-        let offset =
-            buffer_list_box.get_allocated_height() - buffer_list_scroll.get_allocated_height();
-        let paned = &self.paned;
-        let _resize_handler = self.buffer_list.list.connect_size_allocate(
-            clone!(paned => move |_, alloc| {
-                println!("foo");
-                let new_height = alloc.height + offset;
-                gtk::idle_add(clone!(paned => move || {
-                    // paned.set_position(new_height);
-                    gtk::Continue(false)
-                }));
-            }),
-        );
-
-        let list = &self.buffer_list.list;
-        buffer_list_scroll.connect_size_allocate(clone!(list => move |scroll, alloc| {
-        //     let offset = list.get_allocated_height() - alloc.height;
-        //     println!("offset: {:?}", offset);
-            println!("bar");
-        }));
-
-        // paned.connect_button_release_event(clone!(list, buffer_list_scroll => move |_, _| {
-        //     let offset = list.get_allocated_height() - buffer_list_scroll.get_allocated_height();
-        //     println!("offset: {:?}", offset);
-        //     gtk::Inhibit(false)
-        // }));
     }
 }
 
